dc76396eaf00fdd2ddaaac3514f02169299aed57
Damian Malysza <damian.malysza@gmail.com>
2020-11-04 20:28:26 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,13 +4,13 @@ class Triangle
     @l1 = l1
     @l2 = l2
     @l3 = l3
-    len_array = [@l1,@l2,@l3]
+    @len_array = [@l1,@l2,@l3]
   end
 
   def kind
     if (@l1 + @l2 < @l3 || @l2 + @l3 < @l1 || @l1 + @l3 < @l2) || (@l1 <= 0 || @l2 <= 0 || @l3 <=0)
       raise TriangleError
-    elsif @l1 == @l2 && @l2 == @l3
+    elsif len
       :equilateral
     elsif condition
 
