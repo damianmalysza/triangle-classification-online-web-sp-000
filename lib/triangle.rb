a748a33b9f7b8a53d26d2a8b0208e808d333eec4
Damian Malysza <damian.malysza@gmail.com>
2020-11-04 20:36:54 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
   def kind
     if (@l1 + @l2 <= @l3 || @l2 + @l3 <= @l1 || @l1 + @l3 <= @l2) || @len_array.all? {|l| l <=0}
       raise TriangleError
-    elsif @len_array.uniq.size <= 1
+    elsif @len_array.uniq.size == 1
       :equilateral
     elsif @len_array.uniq.size == 3
       :scalene
