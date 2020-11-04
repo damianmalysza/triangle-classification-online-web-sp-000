2bc8ab40bd97755950d6ee6b77cc36a09a64130b
Damian Malysza <damian.malysza@gmail.com>
2020-11-04 20:22:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,6 +9,10 @@ class Triangle
   def kind
     if (@l1 + @l2 < @l3 || @l2 + @l3 < @l1 || @l1 + @l3 < @l2) || (@l1 <= 0 || @l2 <= 0 || @l3 <=0)
       raise TriangleError
+    elsif
+
+    end
+
   end
 
   class TriangleError < StandardError
